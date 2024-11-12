09aba2b0e3db8b7a94560761c249fbb93f37fcad
Louis Yonan <louisyonan@gmail.com>
2024-11-11 17:58:09 -0800

    created code that asks the user to input a number

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        if (args.length != 1) {
+            System.out.println("Please input a number");
+            return;
+        }
     }
 }
